2767e54d90697bfc35ffac881b2e161032dee443
Phuoc Nguyen <hpnvphuoc@gmail.com>
2020-10-19 23:03:47 +0700

    Initial Commit

---
+++ Week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by Phuoc Nguyen on 10/19/20.
+//  Copyright © 2020 Phuoc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
